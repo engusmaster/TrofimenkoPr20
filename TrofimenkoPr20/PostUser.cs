b42ff440dd809dd653536ee38341fc3e853a696b
engusmaster <dtrofimenko.523@gmail.com>
2022-05-19 22:57:54 +0300

    Добавьте файлы проекта.

---
+++ TrofimenkoPr20/PostUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrofimenkoPr20
+{
+    public class PostUser
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int ID { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
---
+++ TrofimenkoPr20/PutUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrofimenkoPr20
+{
+    public class PutUser
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public DateTime UpdateAt { get; set; }
+    }
+}
